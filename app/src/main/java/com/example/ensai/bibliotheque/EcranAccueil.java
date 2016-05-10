b943586294ee7c7ee7edc09a5d3caf07a6abc855
GoujFran <francoise.goujard@laposte.net>
2016-05-10 10:33:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ensai/bibliotheque/EcranAccueil.java
@@ -0,0 +1,13 @@
+package com.example.ensai.bibliotheque;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EcranAccueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecran_accueil);
+    }
+}
