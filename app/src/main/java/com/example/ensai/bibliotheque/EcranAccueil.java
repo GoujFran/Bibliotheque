bce7523e5580f6ed550d762bccf0705d97a170b8
GoujFran <francoise.goujard@laposte.net>
2016-05-10 15:20:56 +0200

    GSON

--- app/src/main/java/com/example/ensai/bibliotheque/EcranAccueil.java
+++ app/src/main/java/com/example/ensai/bibliotheque/EcranAccueil.java
@@ -1,7 +1,19 @@
 package com.example.ensai.bibliotheque;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class EcranAccueil extends AppCompatActivity {
 
@@ -9,5 +21,26 @@ public class EcranAccueil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecran_accueil);
+        //StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+        String contenu = null;
+        try {
+            contenu = run("http://www.omdbapi.com/?t=frozen&y=&plot=short&r=json");
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            Film film = gson.fromJson(contenu, Film.class);
+            Toast.makeText(this,film.toString(),Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Log.e("TAG","Erreur",e);
+        }
+
+    }
+
+    String run(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 }
---
+++ app/src/main/java/com/example/ensai/bibliotheque/Film.java
@@ -0,0 +1,219 @@
+package com.example.ensai.bibliotheque;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by ensai on 10/05/16.
+ */
+public class Film {
+    @SerializedName("Title") private String title;
+    @SerializedName("Year") private String year;
+    @SerializedName("Rated")private String rated;
+    @SerializedName("Released") private String released;
+    @SerializedName("Runtime") private String runtime;
+    @SerializedName("Genre") private String genre;
+    @SerializedName("Director") private String director;
+    @SerializedName("Writer") private String writer;
+    @SerializedName("Actors") private String actors;
+    @SerializedName("Plot") private String plot;
+    @SerializedName("Language") private String language;
+    @SerializedName("Country") private String country;
+    @SerializedName("Awards") private String awards;
+    @SerializedName("Poster") private String poster;
+    @SerializedName("Metascore") private String metascore;
+    private String imdbRating;
+    private String imdbVotes;
+    private String imdbID;
+    @SerializedName("Type") private String type;
+    @SerializedName("Response") private String response;
+
+    public Film() {}
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "title='" + title + '\'' +
+                ", year='" + year + '\'' +
+                ", rated='" + rated + '\'' +
+                ", released='" + released + '\'' +
+                ", runtime='" + runtime + '\'' +
+                ", genre='" + genre + '\'' +
+                ", director='" + director + '\'' +
+                ", writer='" + writer + '\'' +
+                ", actors='" + actors + '\'' +
+                ", plot='" + plot + '\'' +
+                ", language='" + language + '\'' +
+                ", country='" + country + '\'' +
+                ", awards='" + awards + '\'' +
+                ", poster='" + poster + '\'' +
+                ", metascore='" + metascore + '\'' +
+                ", imdbRating='" + imdbRating + '\'' +
+                ", imdbVotes='" + imdbVotes + '\'' +
+                ", imdbID='" + imdbID + '\'' +
+                ", type='" + type + '\'' +
+                ", response='" + response + '\'' +
+                '}';
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setRated(String rated) {
+        this.rated = rated;
+    }
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setAwards(String awards) {
+        this.awards = awards;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public void setMetascore(String metascore) {
+        this.metascore = metascore;
+    }
+
+    public void setImdbRating(String imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public void setImdbVotes(String imdbVotes) {
+        this.imdbVotes = imdbVotes;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getRated() {
+        return rated;
+    }
+
+    public String getReleased() {
+        return released;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getAwards() {
+        return awards;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public String getMetascore() {
+        return metascore;
+    }
+
+    public String getImdbRating() {
+        return imdbRating;
+    }
+
+    public String getImdbVotes() {
+        return imdbVotes;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
